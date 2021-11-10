d4601c06fabb8bf45a1aa89928be7ca2d2614286
Daniel Santos <daniel.santos@jetbridge.com>
2021-11-10 01:37:45 -0300

    feat: add token auth

--- product/urls.py
+++ product/urls.py
@@ -3,6 +3,7 @@
 
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 from product import viewsets
 
@@ -12,4 +13,5 @@ router.register(r'category', viewsets.CategoryViewSet, basename='category')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -3,12 +3,16 @@
 
   
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
 
     def get_queryset(self):
