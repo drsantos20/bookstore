86496038c62ac692edcb7b9fee313b69805d6faf
Daniel Santos <drsantos20@gmail.com>
2022-01-18 01:49:01 -0300

    feat: use format string on bookstore models (#19)

--- product/models/category.py
+++ product/models/category.py
@@ -10,5 +10,5 @@ class Category(models.Model):
     description = models.CharField(max_length=200, blank=True, null=True)
     active = models.BooleanField(default=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
