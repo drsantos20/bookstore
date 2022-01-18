2248e0ae832da0842aec3831f0b4d872b0507b62
Daniel Santos <drsantos20@gmail.com>
2022-01-18 01:48:26 -0300

    feat: use format string on bookstore models

--- product/models/product.py
+++ product/models/product.py
@@ -13,3 +13,7 @@ class Product(models.Model):
     price = models.PositiveIntegerField(null=True)
     active = models.BooleanField(default=True)
     category = models.ManyToManyField(Category, blank=True)
+
+
+    def __str__(self):
+        return self.title
