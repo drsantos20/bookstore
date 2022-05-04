8c8c82f2cd9086466ce1a1b98bd97d9d6c6350bf
Daniel Santos <drsantos20@gmail.com>
2022-05-04 20:25:00 -0300

    fix settings.py changes

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -136,6 +136,17 @@ STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+}
+
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
