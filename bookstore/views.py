7004d721f9dfbe12058678a824ba0d9335548559
Daniel Santos <drsantos20@gmail.com>
2023-09-29 13:50:51 +0000

    fix: pythonanywhere adjustment

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+
+import git
 
 
 @csrf_exempt
 def update(request):
     if request.method == "POST":
         '''
-        pass the path of the diectory where your project will be 
+        pass the path of the diectory where your project will be
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        repo = git.Repo("test.pythonanywhere.com/") 
+        repo = git.Repo('/home/drsantos20/bookstore')
         origin = repo.remotes.origin
+
         origin.pull()
         return HttpResponse("Updated code on PythonAnywhere")
     else:
