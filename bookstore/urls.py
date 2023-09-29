81a93fbc0b5a00bebcbeec37c4b9c06699551d2f
Daniel Santos <drsantos20@gmail.com>
2023-09-28 22:59:02 -0300

    chore: feat deploy to pythonanywhere

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import debug_toolbar
+from bookstore import views
 from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework.authtoken.views import obtain_auth_token
@@ -24,4 +25,5 @@ urlpatterns = [
     re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
+    path("update_server/", views.update, name="update"),
 ]
